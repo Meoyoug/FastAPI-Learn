d241fb0b6075519495feb6abe725c33ce9d2d68a
meoyoug <eodyd426@gmail.com>
2024-09-20 13:06:58 +0900

    Path Parameter 사용법

--- src/main.py
+++ src/main.py
@@ -6,3 +6,9 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {"msg": "Hello World"}
+
+
+# Path Parameter
+@app.get("/greeting/{name}")
+def greeting(name):
+    return {"msg": f"Welcome {name}! Have a Nice Day!"}
