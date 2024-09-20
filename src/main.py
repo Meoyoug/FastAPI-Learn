3f1d7722537317df45b742efdef1666471841c9d
meoyoug <eodyd426@gmail.com>
2024-09-20 13:15:01 +0900

    Query Parameter & Type Annotation 사용법

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "1st"}, {"item_name": "2nd"}, {"item_name": "3rd"}]
+
 
 @app.get("/")
 def index():
@@ -12,3 +14,9 @@ def index():
 @app.get("/greeting/{name}")
 def greeting(name):
     return {"msg": f"Welcome {name}! Have a Nice Day!"}
+
+
+# Query Parameter
+@app.get("/items/")
+def read_item(skip: int = 0, limit: int = 10):  # type annotation
+    return fake_items_db[skip:skip + limit]
