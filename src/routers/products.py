675f44dc7dcc9d92179c09145bc7d62a1de233e3
meoyoug <eodyd426@gmail.com>
2024-09-20 14:44:16 +0900

    Router 사용법 수정

--- src/routers/products.py
+++ src/routers/products.py
@@ -1,19 +1,19 @@
 from typing import Annotated
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/users")
+router = APIRouter(prefix="/products")
 
 fake_items_db = [{"item_name": "1st"}, {"item_name": "2nd"}, {"item_name": "3rd"}]
 
 
 # Query Parameter
-@router.get("/items")
+@router.get("")
 def read_item(skip: int = 0, limit: int = 10):  # type annotation
     return fake_items_db[skip:skip + limit]
 
 
 # Query Parameter
-@router.get("/items/{item_id}")
+@router.get("/{item_id}")
 def search_item(item_id: int, q: str | None = None):  # type annotation
     if q:
         return {"item_id": item_id, "q": q}
@@ -21,6 +21,6 @@ def search_item(item_id: int, q: str | None = None):  # type annotation
 
 
 # Annotated
-@router.get("/products/{name}")
+@router.get("/{name}")
 def get_product(name: Annotated[str, "제품의 이름 입력"]):
     return {"product": name}
