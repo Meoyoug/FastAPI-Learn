9dd0a8af2b9643338cad9e523e38d9c61a3a92a4
meoyoug <eodyd426@gmail.com>
2024-09-20 14:30:05 +0900

    Query, Path Parameter Validation

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from typing import Annotated
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
+
 from schemas import User
 
 app = FastAPI()
@@ -42,3 +43,21 @@ def get_product(name: Annotated[str, "제품의 이름 입력"]):
 def get_user():
     user = User(id='1', name='John Doe', email='test@example.com', password='password')
     return {"user_id": user.id, "user_name": user.name, "user_email": user.email}
+
+
+# Path Parameter Validation
+@app.get("/users/{user_id}")
+def get_user_by_id(user_id: int = Path(gt=0)):
+    user = User(id='1', name='John Doe', email='test@example.com', password='password')
+    if user_id == user.id:
+        return {"user_id": user.id, "user_name": user.name, "user_email": user.email}
+    return {"msg": "User not found"}
+
+
+# Query Parameter Validation
+@app.get("/users/info")
+def get_user_info(user_id: int = Query(default=1, gt=0)):
+    user = User(id='1', name='John Doe', email='test@example.com', password='password')
+    if user_id == user.id:
+        return {"user_id": user.id, "user_name": user.name, "user_email": user.email}
+    return {"msg": "User not found"}
